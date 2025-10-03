c259b68f51dc3df9acc01e34f0282fddd33e5f75
AnaCarol095 <ac.black095@outlook.com>
2025-10-03 20:27:54 -0300

    Add files via upload
    js

---
+++ script.js
@@ -0,0 +1,25 @@
+// Declare uma variável contendo uma string
+let nome = "ana";
+console.log(nome);
+
+// Declare uma variável contendo um número dentro de uma string
+let numeroString = "230";
+console.log(numeroString);
+
+// Declare uma variável com a sua idade
+let idade = 30;
+console.log(idade);
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+let sobrenome = "carolina";
+console.log(nome);
+console.log(sobrenome);
+let nomesobrenome = nome + " " + sobrenome;
+
+// Coloque a seguinte frase em uma variável: It's time
+let frase = "It's time;"
+console.log(frase);
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome)
